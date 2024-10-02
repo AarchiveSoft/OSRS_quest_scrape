d41c13c6ee4f488fc9a707cbe1096cfff357aa86
AaronTheGenerous <aaron@windowslive.com>
2024-10-02 10:52:09 +0200

    Add Selenium setup for Chrome driver
    Introduced Selenium dependencies and setup logic in `scrape.py` for initializing the Chrome driver, including handling for packaged executables. Additionally, updated `.gitignore` to exclude `.idea/` directory.

---
+++ scrape.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import traceback
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+
+
+def setup_driver():
+    if getattr(sys, "frozen", False):
+        # Running as packaged executable, driver is in same directory
+        base_path = sys._MEIPASS
+    else:
+        # Running as normal script, driver is in parent directory
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        base_path = os.path.dirname(base_path)
+    chromedriver_path = os.path.join(base_path, 'chromedriver.exe')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.path.join(base_path, 'chrome', 'win64-118.0.5993.70', 'chrome-win64',
+                                                  'chrome.exe')
+
+    service = Service(chromedriver_path)
+
+    try:
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.get("https://www.graphicart.ch/shop/de/")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
